a45dcb45b95101ccf91e1d9a3517205b66b1dae4
user <qkrdmstjs0709@g.shingu.ac.kr>
2025-05-27 12:28:39 +0900

    미미 변경

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,8 @@
 
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class HelloWorld
  */
-@WebServlet("/hello")
+@WebServlet(description = "날씨도 좋은날 처음 아니 두번째로 만들어보는 서블릿", urlPatterns = { "/hello" })
 public class HelloWorld extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -27,7 +29,8 @@ public class HelloWorld extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		//response.getWriter().append("Served at: ").append(request.getContextPath());
+//		response.getWriter().append("Served at: Inkyu Hwang").append(request.getContextPath());
+		
 		response.setContentType("text/html; charset=utf-8");
 		PrintWriter out = response.getWriter();
 		out.append("<!DOCTYPE html><html><head><title>The first servlet file by me</title></head><body><h2>Hello everytone  Do you hear me?</h2><hr><h2>이 메시지는 이용희교수님이 작성했습니다</h2>   현재시각은");
